5125316f50d5e1859df251318961ab92cd4196d9
Salikhova Nargiza <snargiza.sn@gmail.com>
2021-08-05 19:44:53 +0500

    Прямоугольник

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+       Rect rect=new Rect();
+        rect.setDimens (15,90);
+        System.out.println(rect.perimeter());
+        System.out.println(rect.area ());
+
+    }
+}
---
+++ src/Rect.java
@@ -0,0 +1,12 @@
+public class Rect {
+    double width;
+    double height;
+
+    void setDimens(double width, double height){
+        this.width=width;
+        this.height=height;
+    }
+        double area () {return width * height;}
+
+        double perimeter () {return (width + height) * 2;}
+    }
